a8464bb1cc6045a5b5e7d93200b8760743fd886b
Roshan Poudel <roshan.poudel@stonybrook.edu>
2020-10-12 23:53:19 +0900

    created socket in server.py

---
+++ server.py
@@ -0,0 +1,23 @@
+import socket, threading
+
+HOST = socket.gethostbyname(socket.gethostname()) # Fetches the ip address of the server
+PORT = 8080
+SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Creates a TCP socket to be used over ipv4
+SOCKET.bind((HOST, PORT))  # Binds the ip address of server with the port number
+MESSAGE_CAPACITY = 256 # Maximum Query/Response size in bytes
+
+def handle_connections():
+    """Handles different client connections"""
+    SOCKET.listen()
+    print(f"Server is listening on {HOST}")
+    while True:  # Always keep listening
+        conn, addr = SOCKET.accept()  # Accept a connection
+        # After accepting a connection, handle the client connection
+        # in a new thread
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()  # calls the handle_client method in a new thread
+        print("[ACTIVE CONNECTIONS]: {thread.activeCount() - 1}") # Active connections = Total threads - 1
+
+def handle_client(conn, addr):
+    """Handles each client"""
+    msg = conn.recv(MESSAGE_CAPACITY).decode()
