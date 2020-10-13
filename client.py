b218464467d8ecad5af5affcfdc99217f024ba8b
Roshan Poudel <roshan.poudel@stonybrook.edu>
2020-10-13 10:22:01 +0900

    completed tcp server

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
-import socket, sys
+import socket, sys, struct
 
-HOST = "68.183.131.122"  # Public IP Address of Digital Ocean droplet / Server running server.py
+# HOST = "68.183.131.122"  # Public IP Address of Digital Ocean droplet / Server running server.py
+HOST = "localhost"
 PORT = 8080
 SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Creates a TCP socket to be used over ipv4
 try:
@@ -9,10 +10,29 @@ except:
     print("Error connecting to the server. Check server connection")
     sys.exit(1)
 
-def send():
-    email = input("Enter an email to search in the database")
-    SOCKET.send(b"Q")  # Q denotes this is a query type
-    SOCKET.send(bytes(len(email)))  # sending the length of email
-    SOCKET.send(email.encode("utf-8"))
+def send_message():
+    email = input("Enter an email to search for in the database: ").encode("utf-8")
+    email_length = len(email)
+    msg_type = b"Q"
+    packer = struct.Struct("!cb")
+    header = packer.pack(msg_type, email_length)
+    SOCKET.send(header)  # header contains info about msg type and email length
+    SOCKET.send(email)
+    receive_response(email.decode("utf-8"))
 
-send()
+def receive_response(email):
+    unpacker = struct.Struct("!cb")
+    header_size = struct.calcsize("!cb")
+    msg_type, msg_length = unpacker.unpack(SOCKET.recv(header_size))
+    msg_type = msg_type.decode("utf-8")
+    msg_length = int(msg_length)
+    if msg_type != "R" or msg_length < 0:
+        print("Error: Server sent message using unknown protcol")
+        SOCKET.close()
+        return
+    msg_received = ""
+    for i in range(msg_length):
+        msg_received += SOCKET.recv(1).decode("utf-8")
+    print(f"{email} belongs to {msg_received}")
+
+send_message()
--- server.py
+++ server.py
@@ -1,10 +1,13 @@
-import socket, threading
+import socket, threading, struct
 
-HOST = "68.183.131.122" # Public IP of digital ocean droplet / use your server address
+# HOST = "68.183.131.122" # Public IP of digital ocean droplet / use your server address
+HOST = "localhost"
 PORT = 8080
 SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Creates a TCP socket to be used over ipv4
 SOCKET.bind((HOST, PORT))  # Binds the ip address of server with the port number
-BUFFER_SIZE = 256 # Maximum Query/Response size in bytes
+addrbook = {"luke@gmail.com" : "Luke Skywalker", "jihoon.ryoo@sbu.edu" : "Jihoon Ryoo",
+"abc@gmail.com" : "abc", "roshan.poudel@stonybrook.edu" : "Roshan Poudel"}
+
 
 def handle_connections():
     """Handles different client connections"""
@@ -16,25 +19,39 @@ def handle_connections():
         # in a new thread
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()  # calls the handle_client method in a new thread
-        print("[ACTIVE CONNECTIONS]: {threading.activeCount() - 1}") # Active connections = Total threads - 1
+        print(f"[ACTIVE CONNECTIONS]: {threading.activeCount() - 1}") # Active connections = Total threads - 1
 
 def handle_client(conn, addr):
     """Handles each client"""
-    msg_type = conn.recv(1).decode("utf-8")  # First byte contains message Type
-    str_len = conn.recv(1).decode("utf-8")  # Second byte contains string length
-    if (msg_type != "Q" or not str_len):
+    unpacker = struct.Struct("!cb")
+    header_size = struct.calcsize("!cb")
+    msg_type, msg_length = unpacker.unpack(conn.recv(header_size))
+    msg_type = msg_type.decode("utf-8")
+    msg_length = int(msg_length)
+    if msg_type != "Q" or msg_length < 0:
+        send_message(conn, "Error: Unknown message protocol")
+        conn.close()
         return
-    print(f"Str_len: {str_len}")
-    str_len = int(str_len)
+    email = ""
+    for i in range(msg_length):
+        email += conn.recv(1).decode("utf-8")
+    print(f"MSG Type: {msg_type}")
+    print(f"MSG Length: {msg_length}")
+    print(f"MSG: {email}")
+    print("MSG in dict?: ", (email in addrbook))
+    if email in addrbook:
+        send_message(conn, addrbook[email])
+    else:
+        send_message(conn, "Error: Email not found in database!")
+        conn.close()
 
-    msg = ""
-    while (len(msg) != str_len):
-        msg_chunk = conn.recv(BUFFER_SIZE).decode("utf-8")
-        if msg_chunk == "":
-            break
-        msg += msg_chunk
-        print(msg)
-    conn.close()
+def send_message(conn, msg):
+    msg_type = b"R"
+    packer = struct.Struct("!cb")
+    header = packer.pack(msg_type, len(msg))
+    conn.send(header)
+    conn.send(msg.encode("utf-8"))
+
 
 
 handle_connections()
